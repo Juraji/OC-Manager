43c43b5b10eb1057755cba3ef939a0aa0be76041
Juraji <github@juraji.nl>
2022-07-09 14:28:23 +0200

    Fixed memory image lightbox close btn didn't work; disabled Bootstrap components which are not used; Hack image lightbox module to be 95% of window width (height is limited by Bootstrap)

--- src/web/app/routes/manage-memories/memory-edit/memory-edit.module.ts
+++ src/web/app/routes/manage-memories/memory-edit/memory-edit.module.ts
@@ -6,17 +6,17 @@ import {NgbDatepickerModule, NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap
 
 import {CharacterMgmtModule} from '#components/character-mgmt'
 import {DragDropFileModule} from '#components/drag-drop-file'
-import {ImgSrcFallbackModule} from '#components/img-src-fallback'
+import {ImageLightboxModule} from '#components/image-lightbox/image-lightbox.module'
 import {ReadOnlyFieldModule} from '#components/read-only-field'
 import {DatePipesModule} from '#core/date-pipes'
 import {OcmApiModule} from '#core/ocm-api'
 import {RxUtilPipesModule} from '#core/rx-util-pipes'
 
-import { BaseMemoryFormComponent } from './base-memory-form/base-memory-form.component'
+import {BaseMemoryFormComponent} from './base-memory-form/base-memory-form.component'
 import {MemoryEditComponent} from './memory-edit.component';
 import {MemoryEditResolve} from './memory-edit.resolve';
-import { MemoryEditCharactersComponent } from './memory-edit-characters/memory-edit-characters.component';
-import { MemoryEditImagesComponent } from './memory-edit-images/memory-edit-images.component';
+import {MemoryEditCharactersComponent} from './memory-edit-characters/memory-edit-characters.component';
+import {MemoryEditImagesComponent} from './memory-edit-images/memory-edit-images.component';
 
 const ROUTES: Routes = [
   {
@@ -40,7 +40,7 @@ const ROUTES: Routes = [
     CharacterMgmtModule,
     NgbDropdownModule,
     DragDropFileModule,
-    ImgSrcFallbackModule,
+    ImageLightboxModule,
     OcmApiModule
   ],
   declarations: [
