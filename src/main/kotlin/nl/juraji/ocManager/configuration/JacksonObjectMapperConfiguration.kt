a4f604a0061ed4d893f5b9ff1d67bfad44a000ed
Juraji <github@juraji.nl>
2022-07-10 12:45:05 +0200

    Fix Instant timestamps are skewed in DB

--- src/main/kotlin/nl/juraji/ocManager/configuration/JacksonObjectMapperConfiguration.kt
+++ src/main/kotlin/nl/juraji/ocManager/configuration/JacksonObjectMapperConfiguration.kt
@@ -2,19 +2,13 @@ package nl.juraji.ocManager.configuration
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.*
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
 import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
-import java.time.format.DateTimeFormatter
 
 @Configuration
 class JacksonObjectMapperConfiguration {
@@ -33,6 +27,10 @@ class JacksonObjectMapperConfiguration {
                 MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS,
                 SerializationFeature.WRITE_DATES_WITH_ZONE_ID,
                 DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE,
+            )
+            .featuresToDisable(
+                SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS,
+                DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS
             )
     }
 
